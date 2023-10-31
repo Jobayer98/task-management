54d2df6aee486353ffded433134ea67080fb8a16
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-31 18:41:43 +0600

    add user model

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+require("./config/db");
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ server/src/models/user.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
