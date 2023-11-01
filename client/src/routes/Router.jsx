7dfebb6613c3b992db15ee3d5c677993fa3dabc7
Jobayer98 <joobayerrahman0220@gmail.com>
2023-11-01 11:10:21 +0600

    add signup page

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Signup from "../pages/Auth/Signup";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
