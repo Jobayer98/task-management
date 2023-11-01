debc341cb254521234294418cf9c870123e2866b
Jobayer98 <joobayerrahman0220@gmail.com>
2023-11-01 11:27:27 +0600

    add auth provider

---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const signup = (data) => {
+    console.log(data);
+  };
+  const login = (data) => {
+    console.log(data);
+  };
+
+  const logout = async () => {};
+
+  const userInfo = {
+    user,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
