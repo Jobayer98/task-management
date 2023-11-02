3fe7d9d119893aafdcc273866d6ddd59db584539
Jobayer98 <joobayerrahman0220@gmail.com>
2023-11-02 14:03:11 +0600

    add swagger documentation

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
+const path = require("path");
+
+//swagger
+const file = fs.readFileSync(path.join(__dirname, "../swagger.yml"), "utf8");
+const swaggerDocument = YAML.parse(file);
 
 //internal imports
 const authRoutes = require("./routes/auth.route");
@@ -9,6 +17,7 @@ const taksRoutes = require("./routes/task.route");
 const app = express();
 
 // Middlewares
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json(), cors(), cookieParser());
 
 // Routes
