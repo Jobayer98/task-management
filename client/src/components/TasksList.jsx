b9d2bf8a1329db0b1d57990e318bee8e0dd5de03
Jobayer98 <joobayerrahman0220@gmail.com>
2023-11-02 09:54:08 +0600

    sort by status and show limit

--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -8,20 +8,25 @@ function TasksList() {
   const [tasks, setTasks] = useState([]);
   const [change, setChanged] = useState(false);
   const [showCreateModal, setShowCreateModal] = useState(false);
+  const [perPage, setPerPage] = useState(8);
+  const [status, setStatus] = useState("");
 
   useEffect(() => {
     (async () => {
       await axios
-        .get("http://localhost:3000/api/v1/tasks?limit=8", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
+        .get(
+          `http://localhost:3000/api/v1/tasks?limit=${perPage}&status=${status}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
         .then((response) => {
           setTasks(response.data.tasks);
         });
     })();
-  }, [change]);
+  }, [change, perPage, status]);
 
   const handleIsChangeTask = () => {
     setChanged(!change);
@@ -32,16 +37,46 @@ function TasksList() {
   ));
   return (
     <>
-      {showCreateModal && (
-        <CreateTask
-          setChanged={setChanged}
-          onClose={() => setShowCreateModal(false)}
+      <div>
+        {showCreateModal && (
+          <CreateTask
+            setChanged={setChanged}
+            onClose={() => setShowCreateModal(false)}
+          />
+        )}
+        <Header
+          setTasks={setTasks}
+          showCreateModal={() => setShowCreateModal(true)}
         />
-      )}
-      <Header
-        setTasks={setTasks}
-        showCreateModal={() => setShowCreateModal(true)}
-      />
+      </div>
+      <div className="flex gap-8 justify-end items-center p-4 border-2 mt-2">
+        <div>
+          <label htmlFor="">Show per page</label>
+          <input
+            className="bg-gray-100 rounded outline-none border border-gray-400 ml-2 text-center"
+            value={perPage}
+            type="number"
+            min="1"
+            max="50"
+            onChange={(e) => setPerPage(parseInt(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Status</label>
+          <select
+            className="bg-gray-100 rounded outline-none border border-gray-400 ml-2"
+            value={status}
+            name="status"
+            id=""
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="pending">Pending</option>
+            <option value="in-progress">In Progress</option>
+            <option value="completed">Completed</option>
+          </select>
+        </div>
+      </div>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 p-8">
         {content}
       </div>
